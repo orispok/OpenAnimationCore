8a52387761f35060ebde5198e30ba6bed8706bee
ori <orispok@gmail.com>
2025-09-08 17:06:26 +0300

    refactor: update package structure, implement encryption for content loading, and enhance storage handling

--- ui/src/jsMain/kotlin/com/osg/openanimation/core/ui/webApp.kt
+++ ui/src/jsMain/kotlin/com/osg/openanimation/core/ui/webApp.kt
@@ -3,10 +3,9 @@ package com.osg.openanimation.core.ui
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
 import androidx.navigation.ExperimentalBrowserHistoryApi
-import androidx.navigation.bindToNavigation
+import androidx.navigation.bindToBrowserNavigation
 import com.osg.openanimation.core.ui.preload.PreLoadFallback
 import fadeOutElement
-import kotlinx.browser.window
 import org.jetbrains.skiko.wasm.onWasmReady
 
 @OptIn(ExperimentalBrowserHistoryApi::class, ExperimentalComposeUiApi::class)
@@ -18,7 +17,7 @@ fun webApp(
             PreLoadFallback{
                 fadeOutElement()
                 baseApp.AppEntry {
-                    window.bindToNavigation(it)
+                    it.bindToBrowserNavigation()
                 }
             }
         }
